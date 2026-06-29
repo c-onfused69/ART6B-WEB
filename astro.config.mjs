63aa929d6069cf747429e8ba363e984f2b8a19cd
c-onfused69 <nniloy888@gmail.com>
2026-06-30 01:02:14 +0600

    chore: update tailwindcss to version 3.4.19 and add @tailwindcss/vite dependency; import tailwindcss in index.css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   integrations: [
-    react(),
-    tailwind()
+    react()
   ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
